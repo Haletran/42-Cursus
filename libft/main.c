3cc2aa48ca69c1729d848f227bf3e0ad914af9b5
Baptiste-Pasquier <bpasquier123@gmail.com>
2023-11-01 17:27:38 +0000

    Mise a jour fichiers

--- libft/main.c
+++ libft/main.c
@@ -3,22 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bapasqui <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 16:36:59 by bapasqui          #+#    #+#             */
-/*   Updated: 2023/11/01 17:44:10 by bapasqui         ###   ########.fr       */
+/*   Updated: 2023/11/01 17:11:01 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-int	main()
-{
+int main() 
+{ 
     char str[100] = "Learningisfun"; 
     char *first, *second; 
     first = str; 
     second = str; 
     printf("Original string :%s\n ", str); 
+    memmove(first + 8, first, 10); 
+    printf("memmove : %s\n ", str);  
     ft_memmove(second + 8, first, 10); 
-    printf("memmove overlap : %s\n ", str); 
+    printf("ft_memmove : %s\n ", str); 
+  
+    return 0; 
 }
