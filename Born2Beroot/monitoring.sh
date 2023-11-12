79ca6ab5c9c8341e470ee2064f24d6e48586bf27
Baptiste-Pasquier <bpasquier123@gmail.com>
2023-11-13 00:10:04 +0100

    Update monitoring.sh

--- Born2Beroot/monitoring.sh
+++ Born2Beroot/monitoring.sh
@@ -5,9 +5,10 @@ echo "#CPU physical: " $(nproc --all)
 echo "#vCPU: " $(cat /proc/cpuinfo | grep processor | wc -l)
 echo "#Cpu load: " $(top -bn1 | awk '/Cpu/ { print $2}')"%"
 
-
-## TEST
 echo "Memory Usage: " $(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')
+echo "Disk Usage: "
+echo "CPU load: "
+
 echo "Last boot: " $(last reboot | head - 1)
 
 #GET LWM USAGE
@@ -17,8 +18,11 @@ else
   echo "#LVM use : no"
 fi
 
+echo "#Connection TCP: "
 echo "#User log: " $(ps auxwww | grep sshd | grep pts | wc -l)
 
+echo "#Network: "
+echo "#Sudo: "
 
 
 
