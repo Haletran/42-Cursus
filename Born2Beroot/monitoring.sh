90fae4d05ef1610d24ea54422679a93712c26e67
Baptiste-Pasquier <bpasquier123@gmail.com>
2023-11-12 23:40:23 +0100

    Update monitoring.sh

--- Born2Beroot/monitoring.sh
+++ Born2Beroot/monitoring.sh
@@ -2,4 +2,8 @@
 
 echo "#Architecture:" $(uname -a)
 echo "#CPU physical:" $(nproc --all)
+echo "#vCPU:" $(cat /proc/cpuinfo | grep processor | wc -l)
 
+## TEST
+echo "Memory Usage:" $(cat /proc/meminfo | grep MemAvailable) / $(cat /proc/meminfo/ grep MemTotal)
+echo "Last boot:" $(last reboot | head - 1)
