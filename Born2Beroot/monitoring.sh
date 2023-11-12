b0145960f403b40becaa7f18a24bd635b7298934
Baptiste-Pasquier <bpasquier123@gmail.com>
2023-11-13 00:07:29 +0100

    Update monitoring.sh

--- Born2Beroot/monitoring.sh
+++ Born2Beroot/monitoring.sh
@@ -10,4 +10,15 @@ echo "#Cpu load: " $(top -bn1 | awk '/Cpu/ { print $2}')"%"
 echo "Memory Usage: " $(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')
 echo "Last boot: " $(last reboot | head - 1)
 
+#GET LWM USAGE
+if [ cat /etc/fstab | grep -q "mapper" ]; then 
+  echo "#LVM  use: yes"
+else
+  echo "#LVM use : no"
+fi
+
+echo "#User log: " $(ps auxwww | grep sshd | grep pts | wc -l)
+
+
+
 
