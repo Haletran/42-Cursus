0e713696c16ab42e4ea11ccb6bfee748114df3a6
Baptiste-Pasquier <bpasquier123@gmail.com>
2023-11-12 23:50:09 +0100

    Update monitoring.sh

--- Born2Beroot/monitoring.sh
+++ Born2Beroot/monitoring.sh
@@ -7,7 +7,7 @@ echo "#Cpu load: " $(top -bn1 | awk '/Cpu/ { print $2}')"%"
 
 
 ## TEST
-echo "Memory Usage: " $(cat /proc/meminfo | grep MemAvailable) / $(cat /proc/meminfo/ grep MemTotal)
+echo "Memory Usage: " $(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')
 echo "Last boot: " $(last reboot | head - 1)
 
 
