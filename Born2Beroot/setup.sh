ac8a8590f45b183210506111c571a6e237d42156
Baptiste-Pasquier <bpasquier123@gmail.com>
2023-11-12 22:28:14 +0100

    Update setup.sh

--- Born2Beroot/setup.sh
+++ Born2Beroot/setup.sh
@@ -2,35 +2,30 @@
 
 USERNAME="bapasqui"
 PACKAGES="vim ufw sudo curl git"
-echo "password [success=2 default=ignore] pam_unix.so obscure sha512 minlen=10" >> /etc/pam.d/common-passwordpassword [success=2 default=ignore] pam_unix.so obscure sha512 minlen=8w ssh sudo libpam-pwquality"
 GROUPS="$USERNAME 42"
 
 #BASIC SETUP
-su -
 apt update && apt upgrade -y
 apt install $PACKAGES
 groupadd user42
 usermod -aG sudo bapasqui42
 usermod -aG user42 bapasqui42
-exit
-logout 
 
 #SETUP SSH
 STATUS="$(systemctl is-active sshd.service)"
 if [ "${STATUS}" = "active"]; then
-    echo "PORT 4242" >> /etc/ssh/ssh_config
+    echo "PORT 4242" >> /etc/ssh/sshd_config
 else 
     sudo systemctl enable ssh.service
 	sudo systemctl start ssh.service
-	echo "Port 4242" >> /etc/ssh/ssh_config	  
+	echo "Port 4242" >> /etc/ssh/sshd_config	  
 fi
-
-systemctl restart sshd
+systemctl restart ssh
 
 #SETUP UFW
 echo "IPV6=yes" >> /etc/default/ufw
 sudo ufw default deny incoming
-sudo ufw allow ssh
+sudo ufw allow 4242/tcp
 sudo ufw enable
 
 
@@ -40,5 +35,3 @@ sudo chage --mindays 2 --warndays 7 --maxdays 30 root
 
 sudo cp /etc/pam.d/common-password /etc/pam.d/common-password.bak
 echo "password [success=3 default=ignore] pam_unix.so obscure sha512 minlen=10" >> /etc/pam.d/common-password
-
-#
