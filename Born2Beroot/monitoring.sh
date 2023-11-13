26d749f1c6c4f2ee55ca7db2e39024a16b7f7dcb
Baptiste-Pasquier <bpasquier123@gmail.com>
2023-11-13 14:21:16 +0100

    Update monitoring.sh

--- Born2Beroot/monitoring.sh
+++ Born2Beroot/monitoring.sh
@@ -5,11 +5,9 @@ echo "Broadcast message from $(hostname)@$(whoami) (tty1) $(date):"
 echo "#Architecture: $(uname -a)"
 echo "#CPU physical: $(nproc --all)"
 echo "#vCPU: $(cat /proc/cpuinfo | grep processor | wc -l)"
-echo "#Cpu load: $(top -bn1 | awk '/Cpu/ { print $2}')%"
 
 echo "#Memory Usage: $(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')"
 echo "#Disk Usage: " $(df -h | grep sda1 | awk '{print $3}')"/"$(df -h | grep sda1 | awk '{print $2}') "($(df -h | grep sda1 | awk '{print $5}'))"
-#df -h  # Added to display disk usage
 echo "#CPU load: " $(top -bn1 | awk '/Cpu/ { print $2}')"%"
 echo "#Last boot: $(last reboot | head -n 1 | awk '{print $5, $6, $7, $8}')"
 
