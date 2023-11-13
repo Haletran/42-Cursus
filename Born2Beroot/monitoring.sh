3e7a34f210415f35e371c848566dedd534acb5ab
bpasquier <bpasquier123@gmail.com>
2023-11-13 14:10:33 +0100

    mointoring.sh terminer presque

--- Born2Beroot/monitoring.sh
+++ Born2Beroot/monitoring.sh
@@ -1,30 +1,29 @@
-#!bin/sh
+#!/bin/sh
 
 echo "Broadcast message from $(hostname)@$(whoami) (tty1) $(date):"
 
-echo "#Architecture: " $(uname -a)
-echo "#CPU physical: " $(nproc --all)
-echo "#vCPU: " $(cat /proc/cpuinfo | grep processor | wc -l)
-echo "#Cpu load: " $(top -bn1 | awk '/Cpu/ { print $2}')"%"
+echo "#Architecture: $(uname -a)"
+echo "#CPU physical: $(nproc --all)"
+echo "#vCPU: $(cat /proc/cpuinfo | grep processor | wc -l)"
+echo "#Cpu load: $(top -bn1 | awk '/Cpu/ { print $2}')%"
 
-echo "Memory Usage: " $(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')
-echo "Disk Usage: "
-echo "CPU load: "
+echo "#Memory Usage: $(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')"
+echo "#Disk Usage: " $(df -h | grep sda1 | awk '{print $3}')"/"$(df -h | grep sda1 | awk '{print $2}') "($(df -h | grep sda1 | awk '{print $5}'))"
+#df -h  # Added to display disk usage
+echo "#CPU load: " $(top -bn1 | awk '/Cpu/ { print $2}')"%"
+echo "#Last boot: $(last reboot | head -n 1 | awk '{print $5, $6, $7, $8}')"
 
-echo "Last boot: " $(last reboot | head - 1)
-
-#GET LWM USAGE
-if [ cat /etc/fstab | grep -q "mapper" ]; then 
+# GET LVM USAGE
+if grep -q "mapper" /etc/fstab; then
   echo "#LVM  use: yes"
 else
   echo "#LVM use : no"
 fi
 
-echo "#Connection TCP: "
-echo "#User log: " $(ps auxwww | grep sshd | grep pts | wc -l)
+echo "#Connection TCP: " $(netstat -an | grep -i "tcp" | grep ESTABLISHED | wc -l)
+echo "#User log: $(ps auxwww | grep sshd | grep pts | wc -l)"
 
-echo "#Network: "
-echo "#Sudo: "
-
+echo "#Network: " "IP" $(ip -4 addr show dev eno1 | awk '/inet / {print $2}') $(ip address | grep ether | head -n 1 | awk '{print $2}')
+echo "#Sudo: " $(grep -c 'sudo:' /var/log/sudo/sudo.log | wc -l)
 
 
