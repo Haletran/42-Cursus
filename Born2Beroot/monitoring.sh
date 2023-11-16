cb48b22d27077013e1b1abb10c823b77a53a28ca
Baptiste-Pasquier <bpasquier123@gmail.com>
2023-11-16 21:42:23 +0000

    Amelioration script bar

--- Born2Beroot/monitoring.sh
+++ Born2Beroot/monitoring.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 #OTHERS USEFULS SCRIPTS
-source print_bar.sh
-source
-
+source utils.sh
 
 # ANSI color codes
 RED='\033[0;31m'
@@ -13,85 +11,81 @@ NC='\033[0m'
 
 set -u
 
-#VARIABLES
-let d=$(df -h | grep sda1 | awk '{print $3}' | sed 's/.$//')/1000 
-
-printBar d 1 5
 usage() {
-  echo "Usage: $0 [-a <architecture>] [-c <cpu] ... [--all]"
+  printf "Usage: $0 [-a <architecture>] [-c <cpu] ... [--all]"
 }
 
 while [ $# -gt 0 ]; do
   case "$1" in
     -a|--architecture)
-      echo "${GREEN}Architecture:${NC} $(uname -a)"
+      printf "${GREEN}Architecture:${NC} $(uname -a)"
       ;;
     -c|--cpu-physical)
-      echo "${GREEN}CPU physical:${NC} $(nproc --all)"
+      printf "${GREEN}CPU physical:${NC} $(nproc --all)"
       ;;
     -vc|--vcpu)
       "${GREEN}vCPU:${NC} $(cat /proc/cpuinfo | grep processor | wc -l)"
       ;;
     -m|--memory-usage)
-      echo "${GREEN}Memory Usage:${NC} $(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')"
+      printf "${GREEN}Memory Usage:${NC} $(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')"
       ;;
     -d|--disk-usage)
-      echo "${GREEN}Disk Usage:${NC} $(df -h | grep sda1 | awk '{print $3}')/$(df -h | grep sda1 | awk '{print $2}') ($(df -h | grep sda1 | awk '{print $5}'))"
+      printf "${GREEN}Disk Usage:${NC} $(df -h | grep sda1 | awk '{print $3}')/$(df -h | grep sda1 | awk '{print $2}') ($(df -h | grep sda1 | awk '{print $5}'))"
       ;;
     -cl|--cpu-load)
-      echo "${GREEN}CPU load:${NC} $(top -bn1 | awk '/Cpu/ { print $2}')%"
+      printf "${GREEN}CPU load:${NC} $(top -bn1 | awk '/Cpu/ { print $2}')%"
       ;;
     -lb|--last-boot)
-      echo "${GREEN}Last boot:${NC} $(last reboot | head -n 1 | awk '{print $5, $6, $7, $8}')"
+      printf "${GREEN}Last boot:${NC} $(last reboot | head -n 1 | awk '{print $5, $6, $7, $8}')"
       ;;
     -lvm|--lvm-use)
-	  if lsblk | grep -q "lvm"; then echo "${GREEN}LVM use:${NC} yes"; else echo "${GREEN}LVM use:${NC} no"; fi
+	  if lsblk | grep -q "lvm"; then printf "${GREEN}LVM use:${NC} yes"; else printf "${GREEN}LVM use:${NC} no"; fi
       ;;
     -tc|--tcp-connections)
-      echo "${GREEN}Connection TCP:${NC} $(ss -neopt state established | wc -l)"
+      printf "${GREEN}Connection TCP:${NC} $(ss -neopt state established | wc -l)"
       ;;
     -ul|--user-log)
-      echo "${GREEN}User log:${NC} $(users | wc -w)"
+      printf "${GREEN}User log:${NC} $(users | wc -w)"
       ;;
     -n|--network)
-      echo "${GREEN}Network:${NC} IP $(ip -4 addr show dev eno1 | awk '/inet / {print $2}') $(ip address | grep ether | head -n 1 | awk '{print $2}')"
+      printf "${GREEN}Network:${NC} IP $(ip -4 addr show dev eno1 | awk '/inet / {print $2}') $(ip address | grep ether | head -n 1 | awk '{print $2}')"
       ;;
     -s|--sudo)
-      echo "${GREEN}Sudo:${NC} $(journalctl -q _COMM=sudo | grep COMMAND | wc -l)"
+      printf "${GREEN}Sudo:${NC} $(journalctl -q _COMM=sudo | grep COMMAND | wc -l)"
       ;;
     --all)
       echo "#Architecture: $(uname -a)"
       echo "#CPU physical: $(grep "physical id" /proc/cpuinfo | sort | uniq | wc -l)"
       echo "#vCPU: $(grep "^processor" /proc/cpuinfo | wc -l)"
 	    echo "#Memory Usage: $(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')"
-      mem
+      echo "#Disk Usage: $(df -h | grep sda1 | awk '{print $3}')/$(df -h | grep sda1 | awk '{print $2}') ($(df -h | grep sda1 | awk '{print $5}'))"
       echo "#CPU load: $(top -bn1 | awk '/Cpu/ { print $2}')%"
       echo "#Last boot: $(last reboot | head -n 1 | awk '{print $5, $6, $7, $8}')"
       if lsblk | grep -q "lvm"; then echo "#LVM use: yes"; else echo "#LVM use: no"; fi
       echo "#Connection TCP: $(ss -neopt state established | wc -l)"
       echo "#User log: $(users | wc -w)"
-      echo "#Network: IP $(ip -4 addr show dev enp0s3 | awk '/inet / {print $2}') $(ip address | grep ether | head -n 1 | awk '{print $2}')"
+      echo "#Network: IP $(ip -4 addr show dev eth0 | awk '/inet / {print $2}') $(ip address | grep ether | head -n 1 | awk '{print $2}')"
       echo "#Sudo: $(journalctl -q _COMM=sudo | grep COMMAND | wc -l)"
       ;;
     --allc)
-      echo ${GREEN}$(whoami)${NC}@${GREEN}$(hostname)${NC}
-	  echo "----------------------"
-      echo "${GREEN}Architecture:${NC} $(uname -a)"
-      echo "${GREEN}CPU physical:${NC} $(grep "physical id" /proc/cpuinfo | sort | uniq | wc -l)"
-      echo "${GREEN}vCPU:${NC} $(grep "^processor" /proc/cpuinfo | wc -l)"
-	  echo "${GREEN}Memory Usage:${NC} $(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')"
-      echo "${GREEN}Disk Usage:${NC} $(df -h | grep sda1 | awk '{print $3}')/$(df -h | grep sda1 | awk '{print $2}') ($(df -h | grep sda1 | awk '{print $5}'))"
-      echo "${GREEN}CPU load:${NC} $(top -bn1 | awk '/Cpu/ { print $2}')%"
-      echo "${GREEN}Last boot:${NC} $(last reboot | head -n 1 | awk '{print $5, $6, $7, $8}')"
-      if lsblk | grep -q "lvm"; then echo "${GREEN}LVM use:${NC} yes"; else echo "${GREEN}LVM use:${NC} no"; fi
-      echo "${GREEN}Connection TCP:${NC} $(ss -neopt state established | wc -l)"
-      echo "${GREEN}User log:${NC} $(users | wc -w)"
-      echo "${GREEN}Network:${NC} IP $(ip -4 addr show dev enp0s3 | awk '/inet / {print $2}') $(ip address | grep ether | head -n 1 | awk '{print $2}')"
-      echo "${GREEN}Sudo:${NC} $(journalctl -q _COMM=sudo | grep COMMAND | wc -l)"
-	    echo "----------------------"
+      printf ${GREEN}$(whoami)${NC}@${GREEN}$(hostname)${NC}
+	  printf "\n----------------------\n"
+      printf "${GREEN}Architecture:${NC} $(uname -a)\n"
+      printf "${GREEN}CPU physical:${NC} $(grep "physical id" /proc/cpuinfo | sort | uniq | wc -l)\n"
+      printf "${GREEN}vCPU:${NC} $(grep "^processor" /proc/cpuinfo | wc -l)\n"
+      printf "${GREEN}MemoryUsage:${NC} " && mem
+      printf "${GREEN}Disk Usage: ${NC}%s/%s (%s)" "$(df -h | grep sda1 | awk '{print $3}')" "$(df -h | grep sda1 | awk '{print $2}')" "$(df -h | grep sda1 | awk '{print $5}')"
+      printf "\n${GREEN}CPU load: ${NC}%s" "$(top -bn1 | awk '/Cpu/ { print $2}')"
+      printf "\n${GREEN}Last boot: %s %s${NC}\n" "$(last reboot | head -n 1 | awk '{print $5, $6, $7, $8}')"
+      if lsblk | grep -q "lvm"; then printf "${GREEN}LVM use:${NC} yes\n"; else printf "${GREEN}LVM use:${NC} no\n"; fi
+      printf "${GREEN}Connection TCP:${NC} $(ss -neopt state established | wc -l)\n"
+      printf "${GREEN}User log:${NC} $(users | wc -w)\n"
+      printf "${GREEN}Network:${NC} IP $(ip -4 addr show dev eth0 | awk '/inet / {print $2}') $(ip address | grep ether | head -n 1 | awk '{print $2}')\n"
+      printf "${GREEN}Sudo:${NC} $(journalctl -q _COMM=sudo | grep COMMAND | wc -l)"
+	    printf "\n----------------------\n"
       ;;
     *)
-      echo "${RED}Invalid option: $1${NC}"
+      printf "${RED}Invalid option: $1${NC}"
 	    usage
       exit 1
       ;;
--- Born2Beroot/utils.sh
+++ Born2Beroot/utils.sh
@@ -1,165 +1,18 @@
 #!/bin/bash
 
-set -u
+source print_bar.sh
 
-#ANSI COLORS
-DEF_COLOR='\033[0;39m'
-YELLOW='\033[0;93m'
-BLUE='\033[0;94m'
-CYAN='\033[0;96m'
-NC='\033[0m'
+# ANSI color codes
 RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
 
-#PASSWORD :
-# P@ssw0rd123
-#L1m!t3dP@ss
-#Ch0c0l@t3C@ke
-#R@1nb0w2Sky
-#F1sh1ngT!m3
-#S@lt&Pepp3r1
-#G00dM0rn!ng!
-#B@seb@llL0v3r
-#W1nt3rW@nd3rl@nd
-#H@pp1n3ss&J0y
-#D@nc3InTh3Ra1n
 
-#VARS
-DISTRIB=$(lsb_release -a | grep Di | awk '{print $3}')
 
-usage() {
-  printf "\n${RED}Usage:${NC} $0 [-p1 <part1>] [-p2 <part2> ] [-b <bonus>]\n"
+mem()
+{
+let memmax=$(free -m | grep Mem | awk '{print $2}')/1000
+let memuse=$(free -m | grep Mem | awk '{print $3}')/1000
+echo $(printBar $memuse $memmax 7 ) $(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }') 
 }
-
-pres() {
-  printf ${BLUE}"\n-------------------------------------------------------------\n"${DEF_COLOR};
-  printf ${YELLOW}"\n\t\tSCRIPT CREATED BY: "${DEF_COLOR};
-  printf ${CYAN}"bapasqui\t\n"${DEF_COLOR};
-  printf ${YELLOW}"\t    Github : ${NC}https://github.com/Haletran\t\n"${DEF_COLOR};
-  printf ${BLUE}"\n-------------------------------------------------------------\n"${DEF_COLOR};
-}
-
-while [ $# -gt 0 ]; do
-  case "$1" in
-    -p1 | --part1)
-      if [ "$EUID" -ne 0 ]; then
-        echo "Please run this script as root. (Using this command : su -)"
-        exit 1
-      fi
-      pres
-      # READ USER INPUT
-      read -p $'\e[33mWhat is your username ?\e[0m ' USERNAME
-      printf "${RED}~10 characters long, an uppercase letter, a lowercase letter, \nand a number, not 3 consecutive identical characters.\n${NC}"
-      #read -p "Enter the password for user: " PASSWORD
-      #read -p "Enter the password for root: " ROOT_PASSWORD
-
-      #BASIC SETUP
-      apt-get update && sudo apt-get upgrade -y
-      apt-get install -y sudo
-      if [ $? -eq 0 ]; then echo "sudo installed successfully."; else echo "Failed to install sudo."; fi
-      apt-get install -y ufw vim net-tools libpam-pwquality
-      groupadd user42
-      if ! id "$USERNAME" &>/dev/null; then
-        useradd -m -s /bin/bash "$USERNAME"
-        #echo "$USERNAME:$PASSWORD" | chpasswd 
-        #if [ $? -eq 0 ]; then echo "$USERNAME password changed successfully."; else echo "Failed to change $USERNAME password."; fi
-      #else 
-        #echo "$USERNAME:$PASSWORD" | chpasswd
-        #if [ $? -eq 0 ]; then echo "$USERNAME password changed successfully."; else echo "Failed to change $USERNAME password."; fi
-      fi
-
-      usermod -aG sudo $USERNAME
-      usermod -aG user42 $USERNAME
-      hostnamectl set-hostname $USERNAME"42"
-
-      #CHANGE SUDO PASSWORD
-      #echo "root:$ROOT_PASSWORD" | chpasswd
-      #if [ $? -eq 0 ]; then echo "Root password changed successfully."; else echo "Failed to change root password."; fi
-      cp -r "../Born2Beroot" "/home/$USERNAME/"
-      printf "Logout of root and $USERNAME.\n Login as $USERNAME and execute the second part of the script.\n (sudo ./setup.sh -p2)"
-      ;;
-  -p2 | --part2)
-      if [ "$EUID" -ne 0 ]; then
-        printf "Please run this script as sudo. \n(sudo bash setup.sh <parameters> or sudo ./setup.sh <parameters>)\n"
-        exit 1
-      fi
-      pres
-
-      #GET USERNAME
-      USERNAME=$(whoami)
-      
-      #SETUP UFW
-      echo "IPV6=yes" >> /etc/default/ufw
-      sudo ufw default deny incoming
-      sudo ufw allow 4242/tcp
-      sudo ufw enable
-      
-      #SETUP SSH
-      if (systemctl is-active sshd.service | grep -q "active"); then
-          echo "PORT 4242" >> /etc/ssh/sshd_config
-      else 
-          sudo systemctl enable ssh.service
-          sudo systemctl start ssh.service
-          echo "Port 4242" >> /etc/ssh/sshd_config	  
-      fi
-      echo "PermitRootLogin no" >> /etc/ssh/sshd_config
-      systemctl restart ssh
-      
-      #SETUP PASSWORD EXPIRATION DATE AND POLICIES
-      sudo chage --mindays 2 --warndays 7 --maxdays 30 $USERNAME
-      sudo chage --mindays 2 --warndays 7 --maxdays 30 root
-      sudo mv /etc/pam.d/common-password /etc/pam.d/common-password.bak
-      sudo echo "password        requisite         pam_pwquality.so minlen=10 ucredit=-1 lcredit=-1 dcredit=-1 maxrepeat=3 difok=7 enforce_for_root reject_username" >> /etc/pam.d/common-password
-
-      
-      #SETUP SUDO
-      read -p $'\e[33mCustom Message for failed Sudo password: \e[0m ' MESSAGE
-      mkdir -p /var/log/sudo
-      sudo echo "Defaults    passwd_tries=3" >> /etc/sudoers
-      sudo echo "Defaults    badpass_message="$'\042'"$MESSAGE"$'\042' >> /etc/sudoers
-      sudo echo "Defaults    logfile="$'\042'"/var/log/sudo/sudo.log"$'\042' >> /etc/sudoers
-      sudo echo "Defaults    log_input, log_output" >> /etc/sudoers
-      sudo echo "Defaults    requiretty" >> /etc/sudoers
-      sudo echo "Defaults    secure_path="$'\042'"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin"$'\042' >> /etc/sudoers
-      sudo echo "$USERNAME  ALL=(ALL) NOPASSWD: /usr/local/bin/monitoring.sh" >> /etc/sudoers
-
-      #SETUP CRONJOB
-      mv monitoring.sh /usr/local/bin/monitoring.sh
-      #sudo crontab -u root -e
-      #*/10 * * * * /usr/local/bin/monitoring.sh
-
-      printf "${YELLOW}Your $DISTRIB VM is ready, you might need to change your password if you didn't respect policy${NC}\n"
-      ;;
-  -b | --bonus)
-      if [ "$EUID" -ne 0 ]; then
-        printf "Please run this script as sudo. \n(sudo bash setup.sh <parameters> or sudo ./setup.sh <parameters>)\n"
-        exit 1
-      fi
-      pres
-      echo "Figure out by yourself how to do the bonus part"
-      ;;
-  -t | --test)
-      if [ "$EUID" -ne 0 ]; then
-        echo "Please run this script as root. (Using this command : su -)"
-        exit 1
-      fi
-      #CHECK IF APP ARMOR or SELINUX IS ENABLED (for Debian 10< or for Rocky)
-      if cat /etc/os-release | grep -q Debian; then 
-      	if (cat /sys/module/apparmor/parameters/enabled | grep -q Y); then echo "AppArmor already enabled"; else echo "You need to enable AppArmor"; fi
-      else
-      	if (sestatus | grep -q enabled); then echo "SELINUX already enabled"; else echo "You need to enable SELINUX"; fi
-      fi
-
-      #CLONE AND USE TESTER
-      git clone https://github.com/gemartin99/Born2beroot-Tester.git tester
-      cd tester
-      bash Test.sh
-      ;;
-  *)
-      printf "${RED}Invalid option:${NC} $1"
-	    usage
-      exit 1
-      ;;
-  esac
-  shift
-done
-
