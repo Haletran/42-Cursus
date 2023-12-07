3755581b9d1bb63970742d92a6329d46bc2f77bf
Baptiste-Pasquier <bpasquier123@gmail.com>
2023-12-07 11:00:46 +0000

    Remove tester still the error but half of the tester works

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 16:22:20 by codespace         #+#    #+#             */
-/*   Updated: 2023/12/07 10:52:28 by codespace        ###   ########.fr       */
+/*   Updated: 2023/12/07 10:57:48 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,50 +30,49 @@ char	*ft_free(char *buffer, char *line, char *test)
 }
 static char	*ft_n_copy(char *str)
 {
-	int		i;
-	int		j;
-	char	*test;
+    int		i;
+    int		j;
+    char	*test;
 
-	i = 0;
-	j = 0;
-	while (str[i] != '\n')
-		i++;
-	if (!(test = malloc(ft_strlen(str) - i + 1)))
-		return (NULL);
-	i++;
-	while (str[i] != '\0')
-	{
-		test[j] = str[i];
-		j++;
-		i++;
-	}
-	test[j] = '\0';
-	ft_free(str, NULL, NULL);
-	return (test);
+    i = 0;
+    j = 0;
+    while (str[i] != '\n' && str[i] != '\0')
+        i++;
+    if (!(test = malloc(ft_strlen(str) - i + 1)))
+        return (NULL);
+    i++;
+    while (str[i] != '\0')
+    {
+        test[j] = str[i];
+        j++;
+        i++;
+    }
+    test[j] = '\0';
+    free(str);
+    return (test);
 }
-
 static char	*ft_get_line(char *src)
 {
-	int		i;
-	int		len;
-	char	*dest;
+    int		i;
+    int		len;
+    char	*dest;
 
-	i = 0;
-	len = 0;
-	if (!src)
-		return (NULL);
-	len = ft_nstrlen(src);
-	if (!(dest = malloc(sizeof(*dest) * (len + 2))))
-		return (NULL);
-	while (i < len)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\n';
-	dest[i + 1] = '\0';
-	ft_free(src, NULL, NULL);
-	return (dest);
+    i = 0;
+    len = 0;
+    if (!src)
+        return (NULL);
+    len = ft_nstrlen(src);
+    if (!(dest = malloc(sizeof(*dest) * (len + 2))))
+        return (NULL);
+    while (i < len)
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\n';
+    dest[i + 1] = '\0';
+    free(src);
+    return (dest);
 }
 
 char	*get_next_line(int fd)
