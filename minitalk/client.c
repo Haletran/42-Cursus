95526966fef7fee0d6550c5bea307a8cd0dda9ca
Baptiste-Pasquier <bpasquier123@gmail.com>
2023-12-14 21:44:04 +0000

    MINITALK : modifie et gnlpierre : ajoute

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,11 +6,12 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 13:12:56 by codespace         #+#    #+#             */
-/*   Updated: 2023/12/14 20:31:21 by codespace        ###   ########.fr       */
+/*   Updated: 2023/12/14 21:00:22 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+#include <stdio.h>
 
 void	send_signal(int pid, unsigned char character)
 {
@@ -31,11 +32,32 @@ void	send_signal(int pid, unsigned char character)
 	}
 }
 
+char	binary_to_ascii(const char *binary)
+{
+	int	decimal;
+	int	base;
+	int	i;
+
+	decimal = 0;
+	base = 1;
+	i = 7;
+	while (i >= 0)
+	{
+		if (binary[i] == '1')
+			decimal += base;
+		base *= 2;
+		i--;
+	}
+	return ((char)decimal);
+}
+
 int	main(int argc, char **argv)
 {
 	(void)argc;
 	(void)argv;
-	// find how to convert binary to ascii character
-	send_signal(atoi(argv[1]), (unsigned char)argv[2][0]);
+	char binary[] = "01001000"; // Example binary string
+	char ascii = binary_to_ascii(binary);
+	printf("ASCII character: %c\n", ascii);
+	// send_signal(atoi(argv[1]), ascii);
 	return (0);
 }
