97c00d96232552f8e59f7c84148865dbcb78e625
Baptiste-Pasquier <bpasquier123@gmail.com>
2023-12-21 18:07:22 +0000

    minitalk avec minitalk_bonus

---
+++ minitalk/minitalk_bonus.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk_bonus.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/21 18:06:58 by codespace         #+#    #+#             */
+/*   Updated: 2023/12/21 18:07:14 by codespace        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_BONUS_H
+# define MINITALK_BONUS_H
+
+# include "../libft/libft.h"
+# include <signal.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+#endif
