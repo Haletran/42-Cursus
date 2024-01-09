86df11b29d40f4ee2cb42dfe4648d307e287bb2d
Baptiste-Pasquier <bpasquier123@gmail.com>
2024-01-09 20:58:23 +0000

    add parse.c file and comments and ansi colors.h for later

--- push_swap/src/parse.c
+++ push_swap/src/parse.c
@@ -1,29 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.c                                        :+:      :+:    :+:   */
+/*   parse.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/09 17:00:33 by codespace         #+#    #+#             */
-/*   Updated: 2024/01/09 20:47:20 by codespace        ###   ########.fr       */
+/*   Created: 2024/01/09 20:47:27 by codespace         #+#    #+#             */
+/*   Updated: 2024/01/09 20:52:44 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "includes/push_swap.h"
-
-int	main(int argc, char **argv)
-{
-	t_lst_stack	*i;
-	t_lst_stack	*j;
-
-	i = NULL;
-	j = NULL;
-	if (argc == 0 || (!ft_strchr(argv[1], 32) && ft_strlen(argv[1]) >= 3))
-		return (0);
-	else if (ft_strlen(argv[1]) >= 3 && argc == 2)
-		argv = ft_split(argv[1], ' ');
-	if (verif_input(argc, argv))
-	{
-	}
-}
+// Initialiser le debut de la liste
+// Creer une nouvelle node pour rajouter le contenu (memory allocation for the node)
+// Allouer la memoire pour le nombre
+// Mettre le pointeur de NULL a next newnode->next = NULL
+// Et si c'est la premiere node alors set as head sinon rajouter la node a la suite
