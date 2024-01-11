cf585691b8c46e0fe8c19c46d972222434f9f976
Baptiste-Pasquier <bpasquier123@gmail.com>
2024-01-11 19:36:30 +0000

    we

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 17:00:33 by codespace         #+#    #+#             */
-/*   Updated: 2024/01/11 19:32:00 by codespace        ###   ########.fr       */
+/*   Updated: 2024/01/11 19:36:27 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,13 @@ int	main(int argc, char **argv)
 		init_stack(&a, argc, argv);
 		if (argc == 3 || get_args(argv) == 2)
 			sa(&a);
+		// Why I'm doing this instead of just one function
 /* 		else if (argc == 6 || get_args(argv) == 5)
 			sort_two_nodes(a);
 		else
 			sort_multiples_nodes(a); */
 	}
 	else
+	// TODO: - fix this shitty error if input already sorted
 		return (ft_error());
 }
