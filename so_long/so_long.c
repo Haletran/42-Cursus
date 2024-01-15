1ff1ac5748fcb041ba1d444ccfc10aab9faceb91
Baptiste-Pasquier <bpasquier123@gmail.com>
2024-01-15 16:34:23 +0000

    Macrolibx

--- so_long/so_long.c
+++ so_long/so_long.c
@@ -1,29 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   so_long.h                                          :+:      :+:    :+:   */
+/*   so_long.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/07 15:25:27 by codespace         #+#    #+#             */
-/*   Updated: 2024/01/15 16:33:28 by codespace        ###   ########.fr       */
+/*   Created: 2024/01/15 16:25:12 by codespace         #+#    #+#             */
+/*   Updated: 2024/01/15 16:34:02 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SO_LONG_H
-# define SO_LONG_H
+#include "include/so_long.h"
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1
-# endif
+int	main(int argc, char **argv)
+{
+	if (argc != 2)
+		return (0);
 
-# include "MacroLibX/includes/mlx.h"
-# define WIDTH 1000
-# define HEIGHT 500
+	void *mlx = mlx_init();
+	void *win = mlx_new_window(mlx, 400, 400, "Hello world!");
 
-# include <fcntl.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
-
-#endif
+	mlx_destroy_window(mlx, win);
+	mlx_destroy_display(mlx);
+}
