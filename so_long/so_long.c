cbae96ab5d59cac1c809e673a818fb57f591261a
Baptiste <baptiste@DebianLaptop.home>
2024-01-21 15:46:20 +0100

    Enfin compris comment marche la function key hook et makefile backup

--- so_long/so_long.c
+++ so_long/so_long.c
@@ -6,7 +6,7 @@
 /*   By: baptiste <baptiste@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 16:25:12 by codespace         #+#    #+#             */
-/*   Updated: 2024/01/18 23:24:56 by baptiste         ###   ########.fr       */
+/*   Updated: 2024/01/21 15:45:03 by baptiste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int	key_hook(int key, void *param)
 {
+	mlx_t *mlx;
+	mlx = param;
 	if (key == 41)
 		mlx_loop_end(param);
 	if (key == 4)
@@ -21,7 +23,8 @@ int	key_hook(int key, void *param)
 	if (key == 22)
 		ft_printf("s");
 	if (key == 26)
-		move_player(key);
+		ft_printf("%d", mlx->player_x);
+		//ft_printf("w");
 	if (key == 7)
 		ft_printf("d");
 	return (0);
@@ -32,6 +35,26 @@ int	window_hook(int event, void *param)
 	if (event == 0)
 		mlx_loop_end(param);
 	return (0);
+}
+
+void rendering(mlx_t *mlx)
+{
+	mlx->mlx = mlx_init();
+	mlx->win = mlx_new_window(mlx->mlx, mlx->height*32, mlx->width*32, "So_long");
+	get_var(mlx);
+	render_map(mlx);
+	mlx_on_event(mlx->mlx, mlx->win, MLX_KEYDOWN, key_hook, mlx);
+	mlx_on_event(mlx->mlx, mlx->win, MLX_WINDOW_EVENT, window_hook, mlx);
+	mlx_loop(mlx->mlx);
+
+	// WHEN EXITING WINDOW
+	mlx_destroy_image(mlx->mlx, mlx->wall);
+	mlx_destroy_image(mlx->mlx, mlx->ground);
+	mlx_destroy_image(mlx->mlx, mlx->player);
+	mlx_destroy_image(mlx->mlx, mlx->coin);
+	mlx_destroy_image(mlx->mlx, mlx->exit);
+	mlx_destroy_window(mlx->mlx, mlx->win);
+	mlx_destroy_display(mlx->mlx);
 }
 
 int	main(int argc, char **argv)
@@ -46,30 +69,10 @@ int	main(int argc, char **argv)
 	if (!global_checker(mlx) || !flood_fill(mlx))
 		return (ft_error(3));
 
-	// MAIN MLX FUNCTION WIN
 	int a = 0;
 	while (mlx->map[a] != NULL)
 		printf("%s", mlx->map[a++]);
-	mlx->mlx = mlx_init();
-	mlx->win = mlx_new_window(mlx->mlx, mlx->height*32, mlx->width*32, "So_long");
 
+	rendering(mlx);
 
-
-	render_map(mlx);
-
-	
-	mlx_on_event(mlx->mlx, mlx->win, MLX_KEYDOWN, key_hook, mlx->mlx);
-	mlx_on_event(mlx->mlx, mlx->win, MLX_WINDOW_EVENT, window_hook, mlx->mlx);
-	mlx_loop(mlx->mlx);
-
-	// WHEN EXITING WINDOW
-	mlx_destroy_image(mlx->mlx, mlx->wall);
-	mlx_destroy_image(mlx->mlx, mlx->ground);
-	mlx_destroy_image(mlx->mlx, mlx->player);
-	mlx_destroy_image(mlx->mlx, mlx->coin);
-	mlx_destroy_image(mlx->mlx, mlx->exit);
-	mlx_destroy_window(mlx->mlx, mlx->win);
-	mlx_destroy_display(mlx->mlx);
-	
-	return (0);
 }
--- so_long/src/render.c
+++ so_long/src/render.c
@@ -6,27 +6,31 @@
 /*   By: baptiste <baptiste@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 15:03:50 by bapasqui          #+#    #+#             */
-/*   Updated: 2024/01/18 23:26:36 by baptiste         ###   ########.fr       */
+/*   Updated: 2024/01/21 15:26:05 by baptiste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
-void render_map(mlx_t *data)
+void    get_var(mlx_t *data)
 {
-    int x;
-    int width;
     int height;
-    int y;
+    int width;
 
-    x = 0;
-    y = 0;
 	data->wall = mlx_png_file_to_image(data->mlx, "images/wall.png", &height, &width);
     data->ground = mlx_png_file_to_image(data->mlx, "images/black.png", &height, &width);
     data->exit = mlx_png_file_to_image(data->mlx, "images/exit.png", &height, &width);
     data->player = mlx_png_file_to_image(data->mlx, "images/player.png", &height, &width);
     data->coin = mlx_png_file_to_image(data->mlx, "images/coin.png", &height, &width);
-    
+}
+
+void render_map(mlx_t *data)
+{
+    int x;
+    int y;
+
+    x = 0;
+    y = 0;
     while (data->map[x] != NULL)
     {
         while(data->map[x][y] != '\0')
@@ -49,26 +53,15 @@ void render_map(mlx_t *data)
     
 }
 
-void move_player(int keycode)
+/* void move_player()
 {
     mlx_t *data;
-    int width;
-    int height;
 
     data = malloc(sizeof(mlx_t));
     data = get_co(data);
-
-    data->wall = mlx_png_file_to_image(data->mlx, "images/wall.png", &height, &width);
-    data->ground = mlx_png_file_to_image(data->mlx, "images/black.png", &height, &width);
-    data->exit = mlx_png_file_to_image(data->mlx, "images/exit.png", &height, &width);
-    data->player = mlx_png_file_to_image(data->mlx, "images/player.png", &height, &width);
-    data->coin = mlx_png_file_to_image(data->mlx, "images/coin.png", &height, &width);
-    if (keycode == 26)
-    {
-        mlx_put_image_to_window(data->mlx, data->win, data->player, data->player_y*32, (data->player_x - 1)*32);
-        mlx_put_image_to_window(data->mlx, data->win, data->player, data->player_y*32, data->player_x*32);    
-    }
-}
+    mlx_put_image_to_window(data->mlx, data->win, data->player, data->player_y*32, (data->player_x - 1)*32);
+    mlx_put_image_to_window(data->mlx, data->win, data->player, data->player_y*32, data->player_x*32);    
+} */
 
 
 
