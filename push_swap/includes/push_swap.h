561ae4df0b268c14a1846b459a0c21f85ad93cac
Baptiste-Pasquier <bpasquier123@gmail.com>
2024-01-30 16:34:08 +0000

    Clean Push_Swap

--- push_swap/includes/push_swap.h
+++ push_swap/includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 14:27:56 by codespace         #+#    #+#             */
-/*   Updated: 2024/01/30 16:14:56 by codespace        ###   ########.fr       */
+/*   Updated: 2024/01/30 16:21:54 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,15 @@ typedef struct s_lst
 }				t_lst;
 
 /* FUNCTIONS */
-void pa(t_lst **node1, t_lst **node2);
-void freeList(struct node* head);
+void			pa(t_lst **node1, t_lst **node2);
+void			freeList(struct node *head);
 int				check_input(int nb_args, char **src);
 int				init_stack(t_lst **head, int size, char **arr);
 int				ft_error(void);
 int				ft_strcmp(char *s1, char *s2);
 int				get_args(char **arr);
 int				verif_input(int nb_args, char **src);
+void			sort_small_stack(t_lst *a, t_lst *b);
 
 /*OPERATIONS*/
 void			sa(t_lst **node1);
--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 17:00:33 by codespace         #+#    #+#             */
-/*   Updated: 2024/01/30 16:15:43 by codespace        ###   ########.fr       */
+/*   Updated: 2024/01/30 16:20:05 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ int	main(int argc, char **argv)
 {
 	t_lst	*a;
 
-	// t_lst	*b;
+	t_lst	*b;
 	a = NULL;
-	// b = NULL;
+	b = NULL;
 	if (argc == 1 || (argc == 2 && !argv[1][0]))
 		return (ft_error());
 	else if (argc == 2)
@@ -26,14 +26,10 @@ int	main(int argc, char **argv)
 	if (verif_input(argc, argv))
 	{
 		init_stack(&a, argc, argv);
-		if (argc - 1 == 2 || get_args(argv) == 2)
-			sa(&a);
-		// Why I'm doing this instead of just one function
-/* 		else if (argc == 6 || get_args(argv) == 5)
-			sort_two_nodes(a);
-		else
-			sort_multiples_nodes(a); */
-		freeList(a);
+		if (argc - 1 <= 5 || get_args(argv) <= 5)
+			sort_small_stack(a, b);
+		else if (argc - 1 > 5 || get_args(argv) > 5)
+			sort_big_stack(a, b);
 	}
 	else
 	// TODO: - fix this shitty error if input already sorted
