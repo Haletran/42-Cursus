5afa8bae918e3464b1f2d169f71c66811b5bb923
Haletran <bapasqui@1A5.42angouleme.fr>
2024-02-27 17:23:20 +0100

    Routine add mutex and colors to action

--- philosophers/includes/philo.h
+++ philosophers/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: bapasqui <bapasqui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 12:06:12 by bapasqui          #+#    #+#             */
-/*   Updated: 2024/02/27 13:55:11 by bapasqui         ###   ########.fr       */
+/*   Updated: 2024/02/27 17:09:56 by bapasqui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,5 +71,7 @@ void					init_value(t_table **table, char **arr);
 void					print_table(t_table *table);
 void					*start_dinner(t_table **table);
 void					ft_free_all(t_table **table);
+void	*runtime(void *param);
+
 
 #endif
--- philosophers/src/init.c
+++ philosophers/src/init.c
@@ -6,7 +6,7 @@
 /*   By: bapasqui <bapasqui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 13:00:02 by bapasqui          #+#    #+#             */
-/*   Updated: 2024/02/27 14:59:22 by bapasqui         ###   ########.fr       */
+/*   Updated: 2024/02/27 17:23:01 by bapasqui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,11 @@ void	init_value(t_table **table, char **arr)
 	if (arr[5])
 		(*table)->must_eat = ft_atoi(arr[5]);
 	(*table)->philos->id = malloc(sizeof(t_table) * (*table)->nb_philo + 1);
+	(*table)->philos->right_fork = malloc(100);
+	(*table)->philos->left_fork = malloc(100);
+
+	pthread_mutex_init(&(*table)->philos->right_fork->fork, NULL);
+	pthread_mutex_init(&(*table)->philos->left_fork->fork, NULL);
 }
 
 int	check_valid_time(t_table *table)
@@ -37,25 +42,34 @@ int	check_valid_time(t_table *table)
 	return (1);
 }
 
-static void	*runtime(void *param)
+
+void	*runtime(void *param)
 {
 	t_table		**table;
-	static int	i = 1;
+	int id = 0;
+	static int i = 1;
 
 	table = param;
+	id = (*table)->philos->id[i];
 	while (!(*table)->end_simulation)
 	{
-		printf("philosopher %d is thinking\n", (*table)->philos->id[i]);
+		printf("philosopher %d" YELLOW " is thinking\n" RESET, (*table)->philos->id[id]);
 		usleep(500);
-		printf("philosopher %d is eating\n", (*table)->philos->id[i]);
+
+		pthread_mutex_lock(&(*table)->philos->right_fork->fork);
+		pthread_mutex_lock(&(*table)->philos->left_fork->fork);
+
+		printf("philosopher %d" RED " is eating\n" RESET, (*table)->philos->id[id]);
 		usleep((*table)->time_to_eat);
-		// mutex des fourchettes
-		printf("philosopher %d is sleeping\n", (*table)->philos->id[i]);
+
+		pthread_mutex_unlock(&(*table)->philos->right_fork->fork);
+		pthread_mutex_unlock(&(*table)->philos->left_fork->fork);
+
+		printf("philosopher %d" BLUE " is sleeping\n" RESET, (*table)->philos->id[id]);
 		usleep((*table)->time_to_sleep);
-		// liberer les mutexs
+		i++;
 	}
-	i++;
-	return (NULL);
+	pthread_exit(NULL);
 }
 
 void	create_thread(t_table **table)
@@ -71,7 +85,6 @@ void	create_thread(t_table **table)
 			+ 1);
 	while (i < (*table)->nb_philo + 1)
 	{
-		(*table)->philos->id[i] = id;
 		check = pthread_create(&(*table)->philos->thread_id[i], NULL, runtime,
 				table);
 		if (check)
@@ -79,7 +92,8 @@ void	create_thread(t_table **table)
 			printf("Error creating thread");
 			return ;
 		}
-		usleep(500);
+		(*table)->philos->id[i] = id;
+		usleep(50);
 		id++;
 		i++;
 	}
