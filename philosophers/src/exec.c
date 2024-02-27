ba6372956cad7aa145d058be82d5a06b60bb14bf
Haletran <bapasqui@1A5.42angouleme.fr>
2024-02-27 17:23:45 +0100

    Routine add mutex and colors to action

--- philosophers/src/exec.c
+++ philosophers/src/exec.c
@@ -6,7 +6,7 @@
 /*   By: bapasqui <bapasqui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 11:14:44 by bapasqui          #+#    #+#             */
-/*   Updated: 2024/02/27 14:55:13 by bapasqui         ###   ########.fr       */
+/*   Updated: 2024/02/27 17:23:42 by bapasqui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ void	*start_dinner(t_table **table)
 	while (i < (*table)->nb_philo + 1)
 	{
 		pthread_join((*table)->philos->thread_id[i], NULL);
+		usleep(10);
 		i++;
 	}
 	return (NULL);
