d2ca532ce58adf4bbee18fc7b4d49274f69051ca
Haletran <bpasquier123@gmail.com>
2024-03-18 17:51:25 +0100

    fix sigsev exit if no exit_code

--- minishell/src/built-ins/exit.c
+++ minishell/src/built-ins/exit.c
@@ -6,7 +6,7 @@
 /*   By: bapasqui <bapasqui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 09:16:58 by bapasqui          #+#    #+#             */
-/*   Updated: 2024/03/18 14:01:04 by bapasqui         ###   ########.fr       */
+/*   Updated: 2024/03/18 17:50:48 by bapasqui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,13 @@ int	ft_exit(char *code, t_lst *args)
 	int	exit_code;
 
 	exit_code = 0;
+	if (!code)
+	{
+		printf("exit\n");
+		exit(0);
+	}
 	args->exit_code = ft_atoi(code);
 	exit_code = args->exit_code;
+	printf("exit\n");
 	exit(exit_code);
 }
