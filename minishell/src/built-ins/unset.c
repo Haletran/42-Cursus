5f4d5e95bd1ca2cb04d9a017dd016d3e088e5d7b
Haletran <bpasquier123@gmail.com>
2024-03-18 15:52:24 +0100

    Unset built-in working might have some error to check

--- minishell/src/built-ins/unset.c
+++ minishell/src/built-ins/unset.c
@@ -6,15 +6,58 @@
 /*   By: bapasqui <bapasqui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 10:12:03 by bapasqui          #+#    #+#             */
-/*   Updated: 2024/03/18 14:01:19 by bapasqui         ###   ########.fr       */
+/*   Updated: 2024/03/18 15:51:08 by bapasqui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+int delete(t_lst *args, int len)
+{
+	int		i;
+	int		after_len;
+	char	**cpy;
+
+	after_len = len;
+	i = 0;
+	while (args->env_var[after_len])
+		after_len++;
+	cpy = malloc(sizeof(cpy) * (len + after_len) + 1);
+	while (i < len)
+	{
+		cpy[i] = args->env_var[i];
+		i++;
+	}
+	len++;
+	while (len < after_len)
+	{
+		cpy[i] = args->env_var[len];
+		len++;
+		i++;
+	}
+	args->env_var = cpy;
+	args->env_cpy = cpy;
+	return (SUCCESS);
+}
+
 int	ft_unset(char **str, t_lst *args)
 {
-	(void)args;
-	(void)str;
+	int	len;
+	int i;
+
+	len = 0;
+	i = 1;
+	while (str[i])
+	{
+		len = 0;
+		while (args->env_var[len])
+		{
+			if (!ft_strncmp(args->env_var[len], str[i], ft_strlen(str[1])))
+				break ;
+			len++;
+		}
+		delete (args, len);
+		i++;
+	}
 	return (SUCCESS);
 }
