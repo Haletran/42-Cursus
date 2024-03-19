1a856daa53a2808ed1b49259b88ae2c6126a4ff3
Haletran <bpasquier123@gmail.com>
2024-03-19 17:25:21 +0100

    trying to fix pipes still not working

--- minishell/src/execution/exec_pipe.c
+++ minishell/src/execution/exec_pipe.c
@@ -6,20 +6,18 @@
 /*   By: bapasqui <bapasqui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 09:54:32 by bapasqui          #+#    #+#             */
-/*   Updated: 2024/03/19 16:21:19 by bapasqui         ###   ########.fr       */
+/*   Updated: 2024/03/19 17:22:48 by bapasqui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void    piping(char **str, t_lst *args, char *full_path, int *prev)
+int piping(char **str, t_lst *args, char *full_path, int *prev, int fd[2])
 {
-    int fd[2];
     pid_t   pid;
 
     pipe(fd);
     pid = fork();
-
     if (pid == 0)
     {
         close(fd[0]);
@@ -28,23 +26,19 @@ void    piping(char **str, t_lst *args, char *full_path, int *prev)
         dup2(*prev, STDIN_FILENO);
         close(*prev);
         if (execve(full_path, str, args->env_var) == -1)
-		{
-			perror(full_path);
-			exit(127);
-		}
+        {
+            perror(full_path);
+            exit(127);
+        }
     }
-    else
-    {
-        close(fd[1]);
-        close(*prev);
-        *prev = fd[0];
-        waitpid(pid, &g_value, 0);
-    }
-	signal(CTRL_C, sig_command_is_running);
+    close(fd[1]);
+    close(*prev);
+    *prev = fd[0];
+    signal(SIGINT, sig_command_is_running);
+    return 0;
 }
 
-
-int test_exec(char **str, t_lst **args, int *prev)
+int test_exec(char **str, t_lst **args, int *prev, int fd[2])
 {
     char    *full_path;
     char    *tmp;
@@ -64,36 +58,40 @@ int test_exec(char **str, t_lst **args, int *prev)
         {
             perror(str[i]);
             g_value = 127;
-            return (127);
+            return 127;
         }
     }
     else
     {
         full_path = check_path(str, *args, 0);
         if (full_path == NULL)
-            return (1);
+            return 1;
     }
-    piping(str, *args, full_path, prev);
-    return (0);
+    piping(str, *args, full_path, prev, fd);
+    return 0;
 }
 
 int exec_pipe(char **str, t_lst *args)
 {
+    int fd[2];
     int     i;
     char    **tab;
     int prev;
 
     i = 0;
-    prev = dup (0);
+    prev = dup(STDIN_FILENO);
     while (str[i])
     {
         tab = ft_split(str[i], ' ');
-        test_exec(tab, &args, &prev);
+        test_exec(tab, &args, &prev, fd);
         free_tab(tab);
         tab = NULL;
         i++;
     }
-    wait(NULL);
+    close(fd[1]);
+    dup2(fd[0], STDIN_FILENO);
+    close(fd[0]);
+    //close(prev);
     return (0);
 }
 
